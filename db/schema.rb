e7078a9efe9763ad2bdf1e24f77421fbb5fed072
Filipe Andrade <filipeandrade250693@gmail.com>
2017-08-21 15:43:51 +0100

    validatios for comment and vote added

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,17 @@ ActiveRecord::Schema.define(version: 20170821134410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "video_url"
+    t.string   "content"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.string   "video_url"
@@ -44,5 +55,22 @@ ActiveRecord::Schema.define(version: 20170821134410) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.integer  "comment_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_id"], name: "index_votes_on_comment_id", using: :btree
+    t.index ["post_id"], name: "index_votes_on_post_id", using: :btree
+    t.index ["user_id"], name: "index_votes_on_user_id", using: :btree
+  end
+
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "votes", "comments"
+  add_foreign_key "votes", "posts"
+  add_foreign_key "votes", "users"
 end
