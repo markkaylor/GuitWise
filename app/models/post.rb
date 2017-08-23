1261e93ec0db4afc7a6af0445a38e17e8aeaab44
Mark Kaylor <mkaylor.guitar@gmail.com>
2017-08-23 18:49:23 +0100

    finished the video logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,7 +6,18 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :video_url, presence: true
 
+  after_validation :change_to_embed
+
   def votes_of
     self.votes.sum(:value)
+  end
+
+  def change_to_embed
+    if video_url.include?("embed") == false
+      self.video_url["https://youtu.be/"] = "" if self.video_url.include? "https://youtu.be/"
+      self.video_url["https://www.youtube.com/watch?v="] = "" if self.video_url.include? "https://www.youtube.com/watch?v="
+      self.video_url = "https://www.youtube.com/embed/#{self.video_url}"
+    end
+
   end
 end
