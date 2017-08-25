daeccb38766ff3c169c4adb70a78297e8c4b5584
Mark Kaylor <mkaylor.guitar@gmail.com>
2017-08-25 14:30:37 +0100

    post card comments working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,10 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @post = Post.find(params[:post_id])
+
+    @comment.user = current_user
+    @comment.post = @post
+
     if @comment.save
       redirect_to post_path(@post), :notice => "Your comment has been posted"
     else
@@ -39,6 +43,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:video_url, :content, :id, :post_id)
+    params.require(:comment).permit(:video_url, :content)
   end
 end
