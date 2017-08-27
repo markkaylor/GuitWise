bae49f61e32b34d257e67c4fe3dcd97fe245a392
Mark Kaylor <mkaylor.guitar@gmail.com>
2017-08-27 15:13:16 +0100

    routes changed for security

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,19 @@ Rails.application.routes.draw do
   devise_scope :user do
     root to: 'pages#home'
 
-  resources :users,  only: [:show]
+    resources :users,  only: [:show]
 
-  resources :posts do
-    resources :comments, only: [:new, :create, :edit, :update, :destroy] do
+    resources :posts, only: [ :new, :create, :index, :show ] do
+      resources :comments, only: [:new, :create ] do
+        member do
+          post 'upvote'
+          post 'downvote'
+        end
+      end
       member do
         post 'upvote'
         post 'downvote'
       end
     end
-    member do
-      post 'upvote'
-      post 'downvote'
-    end
   end
-  end
-
-
-
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
