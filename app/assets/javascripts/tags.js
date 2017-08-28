c48447d53bfd2cce502c88169cbff57562675e85
Richy Chen <richychn@hotmail.com>
2017-08-28 11:48:59 +0100

    clickable searchable tags added to post card, not show

--- app/assets/javascripts/tags.js
+++ app/assets/javascripts/tags.js
@@ -1,6 +1,6 @@
 $(function(){
   var tagInput = "";
-  $("#tags input").on({
+  $(".tags input").on({
     focusout : function() {
       var txt = this.value.replace(/[^a-z0-9\+\-\.\#]/ig,''); // allowed characters
       if(txt) $("<span/>", {text:txt.toLowerCase(), insertBefore:this});
@@ -16,20 +16,11 @@ $(function(){
       if(/(188|13|32)/.test(ev.which)) $(this).focusout();
     }
   });
-  $('#tags').on('click', 'span', function() {
+  $('.tags').on('click', 'span', function() {
     var tagInput = $("#post_tag").val();
     tagInput = tagInput.replace($(this).text(), '');
     $("#post_tag").val(tagInput);
     $(this).remove();
 
   });
-  // $('.new-post.form-actions input').blur( function() {
-  //   // $(window).bind('beforeunload', function(){
-  //   var spans = document.getElementsByTagName("span");
-  //   var tagInput = "";
-  //   for(var i = 0; i < spans.length; i++){
-  //     tagInput += ' ' + spans[i];
-  //   };
-  //   $('.tags-hidden').value = tagInput;
-  // });
 });
