a0d06bc7a3949a13697d4d813d29cbee18889c5f
Mark Kaylor <mkaylor.guitar@gmail.com>
2017-08-31 14:28:31 +0100

    fixed stuff

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,10 +9,15 @@ class Comment < ApplicationRecord
 
   def change_to_embed
     if video_url.include?("embed") == false
-      self.video_url["https://youtu.be/"] = "" if self.video_url.include? "https://youtu.be/"
-      self.video_url["https://www.youtube.com/watch?v="] = "" if self.video_url.include? "https://www.youtube.com/watch?v="
-      self.video_url = "https://www.youtube.com/embed/#{self.video_url}"
+      self.video_url = self.video_url.split('/').last if self.video_url.include? "https://youtu.be/"
+      self.video_url = /v=([A-Za-z0-9]*)&?/.match(self.video_url)[1] if self.video_url.include? "https://www.youtube.com/watch?v="
+      self.video_url = "https://www.youtube.com/embed/#{self.video_url}?rel=0"
     end
+
+    if video_url.include?("vimeo") && (video_url.include?("player") == false)
+      self.video_url = "https://player.vimeo.com/video/#{self.video_url.split('/').last}"
+    end
+
   end
 
   def votes_of
